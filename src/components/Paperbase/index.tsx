245645bad18b5ab09aff6ecf6dd392c1eb2631e9
Bruno Padilha Rangel <brpadilha.dev@gmail.com>
2022-04-01 20:12:18 -0300

    feat: clean the archtecture and fix the speaker

---
+++ src/components/Paperbase/index.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { useSpeechSynthesis } from "react-speech-kit";
+import "../../assets/i18n/i18n";
+import Header from "../Header";
+import Content from "../Content"
+import Navigator from "../Navigator";
+import { theme } from "../../config/theme/theme";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
+const drawerWidth = 256;
+
+export default function Paperbase() {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const isSmUp = useMediaQuery(theme.breakpoints.up("sm"));
+  const { speak } = useSpeechSynthesis();
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const handleUserKeyPress = (event: any) => {
+    const { key } = event;
+    console.log(key)
+    speak({ text: key });
+
+  };
+  React.useEffect(() => {
+    window.addEventListener("keydown", handleUserKeyPress);
+  }, []);
+
+  return (
+      <Box sx={{ display: "flex", minHeight: "100vh" }}>
+        <CssBaseline />
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        >
+          {isSmUp ? null : (
+            <Navigator
+              PaperProps={{ style: { width: drawerWidth } }}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+            />
+          )}
+
+          <Navigator
+            PaperProps={{ style: { width: drawerWidth } }}
+            sx={{ display: { sm: "block", xs: "none" } }}
+          />
+
+        </Box>
+        <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <Header onDrawerToggle={handleDrawerToggle} />
+          <Box
+            component="main"
+            sx={{ flex: 1, py: 6, px: 4, bgcolor: "#eaeff1" }}
+          >
+            <Content />
+          </Box>
+          <Box component="footer" sx={{ p: 2, bgcolor: "#eaeff1" }}>
+            <Copyright />
+          </Box>
+        </Box>
+      </Box>
+  );
+}
